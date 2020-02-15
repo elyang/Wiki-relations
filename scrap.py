278b9e2b6cacc22686e406117e0f1c45628ececb
Xiyuan Yang <xiyuanyang@Todds-MBP.attlocal.net>
2020-02-15 01:35:14 -0500

    href has url contains '/wiki'/

--- scrap.py
+++ scrap.py
@@ -15,41 +15,45 @@ soup = BeautifulSoup(url.content, 'html.parser')
 
 # Define functions that locate tags that contain terms we want
 
-# Each term is in an 'a' tag
-def in_a(tag):
-    return tag.name == 'a'
 # Each term has these 2 attributes defined: href and title
 def has_href_and_title(tag):
     return tag.has_attr('href') and tag.has_attr('title') 
 # The tag attribute 'title' is equal to the string enclosed
-def herf_eql_str(tag):
-
+# def herf_eql_str(tag):
+#     title = tag['title']
+#     string = 
 # The url in each term direct to a English Wikipedia page
 def to_wiki_page(tag):
-    base_url = 'https://en.wikipedia.org/wiki'
-    return base_url in tag['href'] 
-
+    if has_href_and_title(tag):
+        base_url = '/wiki/'
+        print(tag['href'])
+        print(type(tag['href']))
+        return tag['href'].startswith(base_url)
+    return False
 
 
 # Find all such tags
-records = soup.find_all(has_href_and_title) # record is a ResultSet object
-print(records)
-
-
+records = soup.find_all(to_wiki_page) 
+# record is a ResultSet object
 
 
 # Loop through all terms to get the name and url
 # Save the records into a Dataframe
 
-records = []
-for tag in :
+for record in records:
 
-    if tag.['title'] !=
-    name = term.get('title')
-    url = term.get('href')
+    print(record.text)
+#     if base_url in link.get('herf'):
 
-    records.append((name, url))
+#         record.content.find_all('a')
 
-#df_coronavirus = pd.Dataframe(records)
-print(records)
-# Exporting as a CSV file
+
+
+#         name = term.get('title')
+#         url = term.get('href')
+
+#     records.append((name, url))
+
+# #df_coronavirus = pd.Dataframe(records)
+# print(records)
+# # Exporting as a CSV file
