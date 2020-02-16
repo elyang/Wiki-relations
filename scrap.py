d42b64ef56ddd47d7497bd3e770bb24c391f8d84
Xiyuan Yang <xiyuanyang@Todds-MBP.attlocal.net>
2020-02-16 16:39:14 -0500

    compare tag title and string enclosed, convert results into a dictionary

--- scrap.py
+++ scrap.py
@@ -9,7 +9,7 @@ import requests
 import pandas as pd
 
 # save Coronavirus Wikipedia page as a "response" object
-url = requests.get('https://en.wikipedia.org/wiki/Coronavirus')
+url = requests.get('https://en.m.wikipedia.org/wiki/Coronavirus')
 
 soup = BeautifulSoup(url.content, 'html.parser')
 
@@ -18,42 +18,37 @@ soup = BeautifulSoup(url.content, 'html.parser')
 # Each term has these 2 attributes defined: href and title
 def has_href_and_title(tag):
     return tag.has_attr('href') and tag.has_attr('title') 
-# The tag attribute 'title' is equal to the string enclosed
-# def herf_eql_str(tag):
-#     title = tag['title']
-#     string = 
+
 # The url in each term direct to a English Wikipedia page
 def to_wiki_page(tag):
     if has_href_and_title(tag):
         base_url = '/wiki/'
-        print(tag['href'])
-        print(type(tag['href']))
         return tag['href'].startswith(base_url)
     return False
-
+# The attribute value of 'title' is equal to the string enclosed by that tag
+def herf_eql_str(tag):
+    if to_wiki_page(tag):
+        return tag['title'].lower() == tag.text[:].lower()
+    return False
 
 # Find all such tags
-records = soup.find_all(to_wiki_page) 
+records = soup.find_all(herf_eql_str) 
 # record is a ResultSet object
-
+# Create a dictionary to store the term: url pairs
+virus_dict = {}
 
 # Loop through all terms to get the name and url
 # Save the records into a Dataframe
-
 for record in records:
-
     print(record.text)
-#     if base_url in link.get('herf'):
+    
+    name = record.text[:]
+    url = record['href']
+    # use a dictionary of terms to remove duplicates
+    virus_dict[name] = url
 
-#         record.content.find_all('a')
-
-
-
-#         name = term.get('title')
-#         url = term.get('href')
-
-#     records.append((name, url))
-
+print(virus_dict)
+    
 # #df_coronavirus = pd.Dataframe(records)
 # print(records)
 # # Exporting as a CSV file
