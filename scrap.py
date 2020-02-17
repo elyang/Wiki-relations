0fdb0b4b678cdea5dc1e5832b3151018f6c71e7a
Xiyuan Yang <xiyuanyang@Todds-MBP.attlocal.net>
2020-02-16 21:51:27 -0500

    include abbreviation terms, check if title in url

--- scrap.py
+++ scrap.py
@@ -14,10 +14,16 @@ url = requests.get('https://en.m.wikipedia.org/wiki/Coronavirus')
 soup = BeautifulSoup(url.content, 'html.parser')
 
 # Define functions that locate tags that contain terms we want
+# We only need terms appeare in the main paragraph inside <p></p>
+# Within the paragraph find its children <a></a>
+def p_and_a(tag):
+    return (tag.name == 'a' and tag.parent.name == 'p')
 
 # Each term has these 2 attributes defined: href and title
 def has_href_and_title(tag):
-    return tag.has_attr('href') and tag.has_attr('title') 
+    if p_and_a(tag):
+        return tag.has_attr('href') and tag.has_attr('title')
+    return False 
 
 # The url in each term direct to a English Wikipedia page
 def to_wiki_page(tag):
@@ -26,13 +32,16 @@ def to_wiki_page(tag):
         return tag['href'].startswith(base_url)
     return False
 # The attribute value of 'title' is equal to the string enclosed by that tag
-def herf_eql_str(tag):
+def herf_eql_str_or_title(tag):
     if to_wiki_page(tag):
-        return tag['title'].lower() == tag.text[:].lower()
+        url = tag['href']
+        new_href = url.replace('_', ' ')
+        return tag['title'].lower() in new_href.lower() or tag['title'].lower() == tag.text[:].lower()  
     return False
 
 # Find all such tags
-records = soup.find_all(herf_eql_str) 
+records = soup.find_all(to_wiki_page)
+
 # record is a ResultSet object
 # Create a dictionary to store the term: url pairs
 virus_dict = {}
